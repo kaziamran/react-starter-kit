0b89e185672444d90c04d370e907459a76921a90
kazi Emran <kaziamran4@gmail.com>
2025-10-17 23:27:11 +0600

    added new section

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,9 @@
-<meta charset="utf-8">
+<meta charset="UTF-8">
+<meta name="description" content="A modern online shop showcasing products with clean and user-friendly designs.">
+<meta name="keywords" content="online shop, dynamic shop card, responsive website">
+<meta name="author" content="Kazi Emran">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-{{-- Inline script to detect system dark mode preference and apply it immediately --}}
 <script>
 (function() {
     const appearance = '{{ $appearance ?? "system" }}';
@@ -16,27 +18,27 @@
 })();
 </script>
 
-{{-- Inline style to set the HTML background color based on our theme in app.css --}}
 <style>
-html {
-    background-color: #fff;
-}
-html.dark {
-    background-color: #fff;
-}
-.nav_section ul {
-    display: flex;
-    align-items: flex-start;
-    gap: 10px;
-    padding-bottom: 20px;
-}
+    html {
+        background-color: #fff;
+    }
+    html.dark {
+        background-color: #fff;
+    }
+    .nav_section ul {
+        display: flex;
+        align-items: flex-start;
+        gap: 10px;
+        padding-bottom: 20px;
+    }
 </style>
-
-<title inertia>{{ config('app.name', 'Laravel') }}</title>
 
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 
-<link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
